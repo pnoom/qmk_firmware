973f170c38f37e3cf4a73072a654f09737773c2d
Andrew Page <ampage24@gmail.com>
2023-06-18 14:49:05 +0100

    Fixed bug in macro by hardcoding for now

--- keyboards/lily58/keymaps/pnoom/keymap.c
+++ keyboards/lily58/keymaps/pnoom/keymap.c
@@ -7,7 +7,7 @@ enum layer_number {
   _ADJUST,
 };
 
-#define MODTAPTMP LT(layer, kc)
+#define MODTAPTMP LT(_LOWER, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
   OSM(MOD_LSFT),  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, OSM(MOD_RSFT),
   KC_CAPS, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  TO(_LOWER),
-            KC_NONUS_BACKSLASH, OSM(MOD_LALT), MO(_MOUSE), MODTAPTMP(_LOWER, KC_SPC), MODTAPTMP(_LOWER, KC_SPC), TO(_MOUSE), OSM(MOD_LCTL), KC_INSERT
+            KC_NONUS_BACKSLASH, OSM(MOD_LALT), MO(_MOUSE), MODTAPTMP, MODTAPTMP, TO(_MOUSE), OSM(MOD_LCTL), KC_INSERT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
